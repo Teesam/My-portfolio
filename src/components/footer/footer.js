14d5050c7ea092f69d5a439dd0e9d8f948a19c37
Olayiwola Samuel <tolayiwola19@yahoo.com>
2021-02-02 00:09:28 +0100

    new update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -23,19 +23,19 @@ const Footer = () => {
                     </div>
 
                     <div>
-                        <a href = '#'>
+                        <a href = 'https://twitter.com/The_Tunde_'>
                             <img src = { Twitter } alt = 'Twitter Logo' />
                         </a>
-                        <a href = '#'>
+                        {/* <a href = '#'>
                             <img src = { Instagram } alt = 'Instagram Logo' />
-                        </a>
-                        <a href = 'mail.yahoo.com'>
+                        </a> */}
+                        <a href = 'tolayiwola19@yahoo.com'>
                             <img src = { Mail } alt = 'Mail Logo' />
                         </a>
-                        <a href = '#'>
+                        <a href = 'https://web.facebook.com/olayiwolatunexsamuel/'>
                             <img src = { Facebook } alt = 'Facebook Logo' />
                         </a>
-                        <a href = '#'>
+                        <a href = 'https://www.linkedin.com/in/olayiwola-samuel-tunde-41281415b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bjr0xS1tATzuFUeHt9YO0bA%3D%3D'>
                             <img src = { Linkedn  } alt = 'Linkedn Logo' />
                         </a>
                         
